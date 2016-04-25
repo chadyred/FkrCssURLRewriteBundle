d3b5b18ffa182a3c0033ba8f880d726b4c2eb819
chadyred <cellier.florian@hotmail.fr>
2016-04-25 19:27:43 +0200

    Fix has

--- Assetic/Filter/CssURLRewriteFilter.php
+++ Assetic/Filter/CssURLRewriteFilter.php
@@ -115,7 +115,7 @@ class CssURLRewriteFilter implements FilterInterface
             try {
                 $request = null;
                 
-                 if ($this->container->has('request_stack')) {
+                 if ( $this->kernel->getContainer()->has('request_stack')) {
                     
                      $request = $this->kernel->getContainer()->get('request_stack')->getCurrentRequest();
                      
